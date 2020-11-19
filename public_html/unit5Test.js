0bdfac9d29d8dac0547981327cbb758f20e44f6f
kiahb <kiahb@Kiahs-Laptop.niacc.edu>
2020-11-19 09:11:43 -0600

    added code

--- public_html/unit5Test.js
+++ public_html/unit5Test.js
@@ -1,4 +1,9 @@
 //Kiah Nicholson-Wilcox
 //this is some beginning text
+let miles = prompt("How many miles per tank of gas?");
+let gallons = prompt("How many gallons does the car hold?");
 
-
+function milesPerGallon(miles, gallons){
+    return miles / gallons;
+}
+document.write("The miles per gallon is "+ milesPerGallon(miles, gallons) + " miles");
